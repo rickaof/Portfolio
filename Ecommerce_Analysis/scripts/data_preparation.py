5e845649d102db7424d55a7ab3a4465e8c2be8d1
rickaof <rick.a.o.f@gmail.com>
2025-04-12 18:24:14 -0300

    The datasets were treated

--- Ecommerce_Analysis/scripts/data_preparation.py
+++ Ecommerce_Analysis/scripts/data_preparation.py
@@ -25,7 +25,7 @@ dfs = {
     for file in files if file in custom_names
 }
 
-# Orders Dataset: treatment and Type Conversion
+# Orders Dataframe: treatment and Type Conversion
 orders = dfs["orders"]  # Extract the 'orders' DataFrame
 
 pd.set_option("display.max_columns", None)
@@ -57,3 +57,118 @@ orders["order_status"] = orders["order_status"].astype("category")
 # Checking if the type conversion was succeessful
 print(orders.dtypes)
 
+# Orders Items Dataframe: treatment and Type Conversion
+
+order_items = dfs["order_items"]  # Extract the 'order_items' DataFrame
+
+pd.set_option("display.max_columns", None)
+print(order_items.head())
+
+# Identifying null or missing values.
+# Check data_preparation.ipynb notebook for treatment details.
+print(order_items.isnull().sum())
+
+# Checking column data types
+print(order_items.dtypes)
+
+# Columns type conversion
+# Converting date columns from object to datetime
+order_items["shipping_limit_date"] = pd.to_datetime(
+    order_items["shipping_limit_date"], errors="coerce")
+
+# Checking if the type conversion was succeessful
+print(order_items.dtypes)
+
+# Customers Dataframe: treatment and Type Conversion
+
+customer = dfs["customer"]  # Extract the 'customers' DataFrame
+
+pd.set_option("display.max_columns", None)
+print(customer.head())
+
+# Identifying null or missing values.
+# Check data_preparation.ipynb notebook for treatment details.
+print(customer.isnull().sum())
+
+# Checking column data types
+# Check data_preparation.ipynb notebook for treatment details.
+print(customer.dtypes)
+
+# Products Dataframe: treatment and Type Conversion
+
+products = dfs["products"]  # Extract the 'products' DataFrame
+
+pd.set_option("display.max_columns", None)
+print(products.head())
+
+# Identifying null or missing values.
+# Check data_preparation.ipynb notebook for treatment details.
+print(products.isnull().sum())
+
+# Null and missing values treatment.
+products["product_category_name"].fillna("Desconhecido", inplace=True)
+num_cols = [
+    "product_name_lenght", "product_description_lenght",
+    "product_photos_qty", "product_weight_g", "product_length_cm",
+    "product_height_cm", "product_width_cm"
+]
+for col in num_cols:
+    products[col].fillna(products[col].median(), inplace=True)
+
+# Checking if the null and missing values were treated.
+print(products.isnull().sum())
+
+# Checking column data types
+# Check data_preparation.ipynb notebook for treatment details.
+print(customer.dtypes)
+
+# Sellers Dataframe: treatment and Type Conversion
+
+sellers = dfs["sellers"]  # Extract the 'sellers' DataFrame
+
+pd.set_option("display.max_columns", None)
+print(sellers.head())
+
+# Identifying null or missing values.
+# Check data_preparation.ipynb notebook for treatment details.
+print(sellers.isnull().sum())
+
+# Checking column data types
+# Check data_preparation.ipynb notebook for treatment details.
+print(sellers.dtypes)
+
+# Payments Dataframe: treatment and Type Conversion
+
+payments = dfs["payments"]  # Extract the 'payments' DataFrame
+
+pd.set_option("display.max_columns", None)
+print(payments.head())
+
+# Identifying null or missing values.
+# Check data_preparation.ipynb notebook for treatment details.
+print(payments.isnull().sum())
+
+# Checking column data types
+print(payments.dtypes)
+
+# Converting 'payment_type' column from object to category.
+print(payments["payment_type"].unique())
+payments["payment_type"] = payments["payment_type"].astype("category")
+
+# Checking if the type conversion was succeessful
+print(payments["payment_type"].dtypes)
+
+# Sellers Geolocation: treatment and Type Conversion
+
+geolocation = dfs["geolocation"]  # Extract the 'geolocation' DataFrame
+
+pd.set_option("display.max_columns", None)
+print(geolocation.head())
+
+# Identifying null or missing values.
+# Check data_preparation.ipynb notebook for treatment details.
+print(geolocation.isnull().sum())
+
+# Checking column data types
+# Check data_preparation.ipynb notebook for treatment details.
+print(geolocation.dtypes)
